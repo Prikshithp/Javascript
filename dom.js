b03ea8a6da25367d3a90eea8bfab4dc712f67766
Prikshit Sharma <153969074+Prikshithp@users.noreply.github.com>
2023-12-19 22:50:25 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,14 @@
+document.getElementById("title")
+document.getElementById("title").id
+document.getElementById("title")..className
+document.getElementById("title").setAttribute('class'),'test')// always overwrite
+title.style.backgroundcolor='red'
+// types of content
+1> title.innerHtml
+.innertext
+.textcontent
+
+// different query selector
+1> document.querySelector('h2') // selects the first h2 from the page
+2> ('#title') -> for id 
+3> ('.class') -> for class
