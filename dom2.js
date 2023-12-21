3d36cabe1fdbca935590cbaecb8b775ed26905f3
Prikshit Sharma <153969074+Prikshithp@users.noreply.github.com>
2023-12-21 23:48:11 +0530

    Create dom2.js

---
+++ dom2.js
@@ -0,0 +1,7 @@
+ const parent =document.querySelector('.parent');
+parent.children > gives html collection
+parent.children[0]
+for(let i=0;i<parent.children.length;i++){
+  log(parent.children[i].innerhtml;
+}
+log(parent.firsteleemntchild);
