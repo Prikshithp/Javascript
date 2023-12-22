69eb221333a2b1707cee55ea3a5fb91bf6fe15c5
Prikshit Sharma <153969074+Prikshithp@users.noreply.github.com>
2023-12-22 18:00:21 +0530

    Create dom3.js

---
+++ dom3.js
@@ -0,0 +1,8 @@
+document.createElement('div')
+        console.log(div);
+        div.className="main"
+        div.id=Math.random() * 10;
+        div.setAttribute("title","generated title")
+        div.style.padding="12px"
+        div.style.backgroundcolor="green"
+        div.innerText="chai or code";
