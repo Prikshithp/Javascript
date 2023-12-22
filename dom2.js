26fe7a41d4f2ccca66e8c934831885a689035a9a
Prikshit Sharma <153969074+Prikshithp@users.noreply.github.com>
2023-12-22 17:59:45 +0530

    Update dom2.js

--- dom2.js
+++ dom2.js
@@ -1,7 +1,13 @@
- const parent =document.querySelector('.parent');
-parent.children > gives html collection
+ const parent =document.querySelector('.parent'); // class kri select
+parent.children > gives html collection almost array like property
 parent.children[0]
 for(let i=0;i<parent.children.length;i++){
   log(parent.children[i].innerhtml;
 }
 log(parent.firsteleemntchild);
+parent.lastElementChild
+// select parent from child
+const dayone = document.querySelector('.day')
+dayone.parentelemt
+dayone.nextelementsibling
+log(parent.childnodes) // sirf monay tuesday ni hona usme div, gap sb hoga nodelist me
